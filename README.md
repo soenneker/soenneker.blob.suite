7b3c0cec4e05ee0b1c5186499ae4c3730e64115f
Jake Soenneker <jake@soenneker.com>
2023-04-13 19:46:15 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Standby...
+[![](https://img.shields.io/nuget/v/Soenneker.Blob.Suite.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Blob.Suite/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.blob.suite/publish.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.blob.suite/actions/workflows/publish.yml)
+[![](https://img.shields.io/nuget/dt/Soenneker.Blob.Suite.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Blob.Suite/)
+
+# ![](https://user-images.githubusercontent.com/4441470/224455560-91ed3ee7-f510-4041-a8d2-3fc093025112.png) Soenneker.Blob.Suite
+### A concoction of Azure Blob Storage utilities and libraries
+
+## Installation
+
+```
+Install-Package Soenneker.Blob.Suite
+```
---
+++ src/Registrars/BlobSuiteRegistrar.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Soenneker.Blob.Copy.Registrars;
+using Soenneker.Blob.Delete.Registrars;
+using Soenneker.Blob.Download.Registrars;
+using Soenneker.Blob.Service.Registrars;
+using Soenneker.Blob.Upload.Registrars;
+
+namespace Soenneker.Blob.Suite.Registrars;
+
+/// <summary>
+/// A concoction of Azure Blob Storage utilities and libraries
+/// </summary>
+public static class BlobSuiteRegistrar
+{
+    /// <summary>
+    /// Adds all of the Azure Cosmos utilities needed for use <para/>
+    /// </summary>
+    /// <param name="services"></param>
+    public static void AddBlobSuite(this IServiceCollection services)
+    {
+        services.AddBlobDownloadUtil();
+        services.AddBlobUploadUtilAsSingleton();
+        services.AddBlobCopyAsSingleton();
+        services.AddBlobDeleteUtilAsSingleton();
+        services.AddBlobServiceUtilAsSingleton();
+    }
+}
